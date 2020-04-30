35325d4961568ec575f035f18d1e687c2fa3ccc6
nealder2 <64615853+nealder2@users.noreply.github.com>
2020-04-30 23:44:13 +0200

    Initial commit

---
+++ src/test/java/hu/unideb/inf/StepDefinitions.java
@@ -0,0 +1,172 @@
+package hu.unideb.inf;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class StepDefinitions {
+
+    static ChromeDriver driver;
+
+    static {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Given("The home page is opened")
+    public void theHomePageIsOpened() {
+        driver.get("http://automationpractice.com/");
+    }
+
+    @And("The Sign In Link clicked")
+    public void theSignInLinkClicked() {
+        driver.findElement(By.className("login")).click();
+    }
+
+    @Given("The Sign In button is clicked")
+    public void theSignInButtonIsClicked() {
+        driver.findElement(By.id("SubmitLogin")).click();
+    }
+
+    @Then("An email address required error message is shown")
+    public void anEmailAddressRequiredErrorMessageIsShown() {
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"center_column\"]/div[1]/ol/li"));
+        Assert.assertNotEquals(0, elements.size());
+        Assert.assertEquals("An email address required.", elements.get(0).getText());
+    }
+
+    @Given("The {string} is filled with {string}")
+    public void theFieldIsFilledWithValue(String field, String value) {
+        driver.findElement(By.id(field)).sendKeys(value);
+    }
+
+    @Then("An {string} error is shown")
+    public void anMsgErrorIsShown(String msg) {
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"center_column\"]/div[1]/ol/li"));
+        Assert.assertNotEquals(0, elements.size());
+        Assert.assertEquals(msg, elements.get(0).getText());
+    }
+
+    @Given("The credentials are filled correctly")
+    public void theCredentialsAreFilledCorrectly() {
+        driver.findElement(By.id("email")).sendKeys("nealder@freemail.hu");
+        driver.findElement(By.id("passwd")).sendKeys("qwe123");
+    }
+
+    @Then("Login is successful")
+    public void loginIsSuccessful() {
+        Assert.assertEquals("MY ACCOUNT", driver.findElement(By.className("page-heading")).getText());
+    }
+
+    @And("Sign Out button is clicked")
+    public void signOutButtonIsClicked() {
+        driver.findElement(By.className("logout")).click();
+    }
+
+    @Then("Login Page is shown")
+    public void loginPageIsShown() {
+        Assert.assertEquals("AUTHENTICATION", driver.findElement(By.className("page-heading")).getText());
+    }
+
+    @And("The {string} button is clicked")
+    public void theBtnButtonIsClicked(String btn) {
+        driver.findElement(By.xpath(String.format("//a[@title='%s']", btn))).click();
+    }
+
+    @Then("The {string} is shown")
+    public void theContentIsShown(String content) {
+        String pageHead = driver.findElement(By.xpath("//*[@id=\"center_column\"]/h1")).getText();
+
+
+        Assert.assertEquals(content, pageHead);
+
+        signOutButtonIsClicked();
+        loginPageIsShown();
+    }
+
+    @And("The other {string} is shown")
+    public void theOtherContentIsShown(String content) {
+        String pageHead = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/h1")).getText();
+
+        Assert.assertEquals(content, pageHead);
+
+        signOutButtonIsClicked();
+        loginPageIsShown();
+    }
+
+    @Given("An empty search string")
+    public void anEmptySearchString() {
+        driver.findElement(By.id("search_query_top")).sendKeys(Keys.ENTER);
+    }
+
+    @Then("Zero result page is shown")
+    public void zeroResultPageIsShown() {
+        Assert.assertEquals("Please enter a search keyword", driver.findElement(By.xpath("//*[@id=\"center_column\"]/p")).getText());
+    }
+
+    @Given("Actual search term entered")
+    public void actualSearchTermEntered() {
+        driver.findElement(By.id("search_query_top")).sendKeys("Faded" + Keys.ENTER);
+    }
+
+    @And("The search result is found")
+    public void theSearchResultIsFound() {
+        //String element = driver.findElement(By.xpath("//*[@id=\"center_column\"]/h1/span[2]")).getText();
+        String element = driver.findElement(By.className("heading-counter")).getText();
+
+        Assert.assertNotEquals("0 result has been found.", element);
+    }
+
+    @And("The search result is clicked")
+    public void theSearchResultIsClicked() {
+        driver.findElement(By.xpath("//img[@title=\"Faded Short Sleeve T-shirts\"]")).click();
+    }
+
+    @Then("The item is shown")
+    public void theItemIsShown() {
+        String content = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/div/div[3]/h1")).getText();
+
+        Assert.assertEquals("Faded Short Sleeve T-shirts", content);
+    }
+
+    @Then("Add item to wish")
+    public void addItemToWish() {
+        driver.findElement(By.xpath("//*[@id=\"wishlist_button\"]")).click();
+    }
+
+    @Then("Check item in wish")
+    public void checkItemInWish() {
+        String quantity = driver.findElement(By.xpath("//*[@id=\"wishlist_18803\"]/td[2]")).getText();
+        Assert.assertNotEquals(0, quantity);
+
+
+    }
+
+    @Then("Click account")
+    public void clickAccount() {
+        driver.findElement(By.className("account")).click();
+    }
+
+    @Then("Click wishlist")
+    public void clickWishlist() {
+        driver.findElement(By.xpath("//a[@title=\"My wishlists\"]")).click();
+    }
+
+    @Then("Delete item from wish")
+    public void deleteItemFromWish() {
+        driver.findElement(By.xpath("//*[@id=\"wishlist_18803\"]/td[6]/a/i")).click();
+    }
+}
---
+++ src/test/resources/hu/unideb/inf/BencsDaniTeszt.feature
@@ -0,0 +1,69 @@
+Feature: YourLogo login process
+
+  Background:
+    Given The home page is opened
+    And The Sign In Link clicked
+
+  Scenario:
+    Given The Sign In button is clicked
+    Then An email address required error message is shown
+
+  Scenario Outline:
+    Given The '<field>' is filled with '<value>'
+    And The Sign In button is clicked
+    Then An '<msg>' error is shown
+    Examples:
+      | field | value             | msg                    |
+      | email | invalid.email.com | Invalid email address. |
+      | email | valid@email.com   | Password is required.  |
+
+  Scenario:
+    Given The credentials are filled correctly
+    And The Sign In button is clicked
+    And Login is successful
+    And Sign Out button is clicked
+    Then Login Page is shown
+
+  Scenario Outline:
+    Given The credentials are filled correctly
+    And The Sign In button is clicked
+    And Login is successful
+    And The '<btn>' button is clicked
+    And The '<content>' is shown
+    Examples:
+      | btn           | content                   |
+      | Orders        | ORDER HISTORY             |
+      | Credit slips  | CREDIT SLIPS              |
+      | Addresses     | MY ADDRESSES              |
+
+  Scenario Outline:
+    Given The credentials are filled correctly
+    And The Sign In button is clicked
+    And Login is successful
+    And The '<btn>' button is clicked
+    And The other '<content>' is shown
+    Examples:
+      | btn           | content                   |
+      | My wishlists  | MY WISHLISTS              |
+      | Information   | YOUR PERSONAL INFORMATION |
+
+  Scenario:
+    Given An empty search string
+    Then Zero result page is shown
+
+  Scenario:
+    Given Actual search term entered
+    And The search result is found
+    And The search result is clicked
+    Then The item is shown
+
+  Scenario:
+    Given Actual search term entered
+    And The search result is found
+    And The search result is clicked
+    Then The item is shown
+    Then Add item to wish
+    Then Click account
+    Then Click wishlist
+    Then Check item in wish
+    Then Delete item from wish
